2a270bf19963c546b8f764c5ead48866322a2bde
crazyguy-67 <crazy2hell67@gmail.com>
2026-05-29 14:09:06 +0530

    add models

---
+++ src/models/todo.model.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const todoSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    description: {
+      type: String,
+      default: "",
+      trim: true,
+    },
+
+    completed: {
+      type: Boolean,
+      default: false,
+    },
+
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+module.exports = mongoose.model("Todo", todoSchema);
